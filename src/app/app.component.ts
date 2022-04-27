5708309d10c545e9b7415914a91d6592f068a760
Alejo10202 <pipe0189@gmail.com>
2022-04-27 17:59:32 -0500

    otra prueba de angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,29 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'pruebas';
+
+// PROPIEDADES  
+  empresa="Google";
+  usuRegistrado=false;
+  TextoDeRegistro="no hay nadie registrado";
+
+// FUNCIONES
+
+  llamaEmpresa(value:string){
+    
+  }
+
+  /*getRegistroUsuario(){
+    this.usuRegistrado=false
+  }*/
+
+  setUsuarioRegistrado(event:Event){
+    //alert("el usuario se registro")
+    //this.TextoDeRegistro="el usuario se registro"
+    if((<HTMLInputElement>event.target).value== "si"){
+      this.TextoDeRegistro="el usuario se registro";
+    }else{
+      this.TextoDeRegistro="no hay nadie registrado";
+    }
+  }
 }
